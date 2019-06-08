060178f2f7e87eb4d62248de3bf217c991ee7e3a
amendonca <mendonca.andre@outlook.com>
2019-06-08 14:58:41 -0400

    NiFi.Swagger 1.9.1

--- NiFi.Swagger/Properties/AssemblyInfo.cs
+++ NiFi.Swagger/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Swagger Library")]
-[assembly: AssemblyDescription("A library generated from a Swagger doc")]
+[assembly: AssemblyTitle("NiFi.Swagger")]
+[assembly: AssemblyDescription(".NET Client for Apache NiFi 1.9.1 using swagger-cogeden")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Swagger")]
-[assembly: AssemblyProduct("SwaggerLibrary")]
-[assembly: AssemblyCopyright("No Copyright")]
+[assembly: AssemblyCompany("NiFi.Swagger")]
+[assembly: AssemblyProduct("NiFi.Swagger")]
+[assembly: AssemblyCopyright("NiFi.Swagger")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -28,7 +28,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("1.9.1")]
+[assembly: AssemblyFileVersion("1.9.1")]
 [assembly: Guid("6A5CB7AF-F143-4C7C-989C-5BF29C76D615")]
 
