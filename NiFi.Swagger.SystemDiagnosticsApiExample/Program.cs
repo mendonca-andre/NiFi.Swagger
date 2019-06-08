4d2191582be4f4871e5d5ebfe30d0a408b543c48
amendonca <mendonca.andre@outlook.com>
2019-06-08 13:22:42 -0400

    Initial commit.

---
+++ NiFi.Swagger.SystemDiagnosticsApiExample/Program.cs
@@ -0,0 +1,34 @@
+﻿
+namespace NiFi.Swagger.SystemDiagnosticsApiExample
+{
+    using System;
+
+    using NiFi.Swagger.Api;
+    using NiFi.Swagger.Client;
+    using Console = System.Console;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var config = new Configuration
+                {
+                    BasePath = "http://localhost:8080/nifi-api"
+                };
+
+                var systemApi = new SystemDiagnosticsApi(config);
+
+                var localDiagnostics = systemApi.GetSystemDiagnostics();
+                Console.WriteLine(localDiagnostics.ToJson());
+
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
